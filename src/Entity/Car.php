6f98280b6e30388cb1edb5641f8b3127d10aaf82
AniTecK <aniteck82@hotmail.fr>
2020-02-09 11:53:39 +0100

    added car type attribute

--- src/Entity/Car.php
+++ src/Entity/Car.php
@@ -66,6 +66,11 @@ class Car
      */
     private $thumbnail;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -187,6 +192,18 @@ class Car
     public function setThumbnail(string $thumbnail): self
     {
         $this->thumbnail = $thumbnail;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
 
         return $this;
     }
